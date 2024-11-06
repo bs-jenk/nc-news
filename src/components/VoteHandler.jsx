27f77f777c058a084a5e79c1bb9eb4be97aa13b8
bs-jenk <jenkinson.sam@gmail.com>
2024-11-06 17:06:32 +0000

    add functionality for voting on articles

--- src/components/VoteHandler.jsx
+++ src/components/VoteHandler.jsx
@@ -1,10 +1,34 @@
-const VoteHandler = ({ votes }) => {
+import { useEffect, useState } from "react";
+import { updateVotesByArticleId } from "../utils/api";
+
+const VoteHandler = ({ article_id, votes }) => {
+  const [articleVotes, setArticleVotes] = useState(votes);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isIncreased, setIsIncreased] = useState(true);
+
+  useEffect(() => {
+    updateVotesByArticleId(article_id, articleVotes).then((updatedArticleData) => {
+      console.log(updatedArticleData.votes, "<< db votes")
+    })
+  }, [articleVotes])
+
+  const incrementVotes = () => {
+    setArticleVotes(articleVotes + 1);
+    setIsDisabled(true);
+    setIsIncreased(true);
+  }
+
+  const decrementVotes = () => {
+    setArticleVotes(articleVotes - 1);
+    setIsDisabled(true);
+    setIsIncreased(false);
+  }
 
   return (
     <section className="votes-section">
-      <h4>Votes: {votes}</h4>
-      <button className="vote-btns">Vote +1</button>
-      <button className="vote-btns">Vote -1</button>
+      <h4>Votes: {articleVotes}</h4>
+      <button className="vote-btns" onClick={incrementVotes} disabled={isDisabled}>Vote +1</button>
+      <button className="vote-btns" onClick={decrementVotes} disabled={isDisabled}>Vote -1</button>
     </section>
   )
 }
