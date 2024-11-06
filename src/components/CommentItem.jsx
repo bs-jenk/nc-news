8b448ba5f1f389b1526fa0997ec43c0392967d63
bs-jenk <jenkinson.sam@gmail.com>
2024-11-06 12:07:53 +0000

    make comments more readable

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,6 +1,12 @@
 const CommentItem = ({ comment }) => {
 
-  return <p className="comment">"{comment.body}" <span><strong>- {comment.author}</strong> at {comment.created_at}</span></p>
+  return (
+    <>
+      <p><strong>{comment.author}</strong> at {comment.created_at}:</p>
+      <p className="comment">"{comment.body}"</p>
+    </>
+  )
+
 }
 
 export default CommentItem;
