9f42544bf209d33df3c34b80536ebe27d3228f79
bs-jenk <jenkinson.sam@gmail.com>
2024-11-07 19:40:22 +0000

    render a delete button for each comment posted by the signed in user

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import UserContext from "../contexts/userContext";
+
 const CommentItem = ({ comment }) => {
+  const { signedInUser } = useContext(UserContext);
 
   return (
     <li>
       <p><strong>{comment.author}</strong> at {comment.created_at}:</p>
+      {signedInUser === comment.author ? <button>Delete comment</button> : null}
       <p className="comment">"{comment.body}"</p>
     </li>
   )
