9d3dcf074592d3cb1dc311d06eef27da33f392a6
bs-jenk <jenkinson.sam@gmail.com>
2024-11-07 09:06:14 +0000

    add error handling for voting

--- src/components/VoteHandler.jsx
+++ src/components/VoteHandler.jsx
@@ -4,24 +4,24 @@ import { updateVotesByArticleId } from "../utils/api";
 const VoteHandler = ({ article_id, votes }) => {
   const [articleVotes, setArticleVotes] = useState(votes);
   const [isDisabled, setIsDisabled] = useState(false);
-  const [isIncreased, setIsIncreased] = useState(true);
+  const [errMsg, setErrMsg] = useState('');
 
   useEffect(() => {
-    updateVotesByArticleId(article_id, articleVotes).then((updatedArticleData) => {
-      console.log(updatedArticleData.votes, "<< db votes")
-    })
-  }, [articleVotes])
+    updateVotesByArticleId(article_id, articleVotes)
+      .catch(() => {
+        setIsDisabled(true);
+        setErrMsg('Error - please try voting again later');
+      })
+  }, [articleVotes]);
 
   const incrementVotes = () => {
     setArticleVotes(articleVotes + 1);
     setIsDisabled(true);
-    setIsIncreased(true);
   }
 
   const decrementVotes = () => {
     setArticleVotes(articleVotes - 1);
     setIsDisabled(true);
-    setIsIncreased(false);
   }
 
   return (
@@ -29,6 +29,7 @@ const VoteHandler = ({ article_id, votes }) => {
       <h4>Votes: {articleVotes}</h4>
       <button className="vote-btns" onClick={incrementVotes} disabled={isDisabled}>Vote +1</button>
       <button className="vote-btns" onClick={decrementVotes} disabled={isDisabled}>Vote -1</button>
+      <p id="vote-err-msg">{errMsg}</p>
     </section>
   )
 }
