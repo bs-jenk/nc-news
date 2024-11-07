32d393d458ae2f4f92aa5a80278aa1aca75c0d34
bs-jenk <jenkinson.sam@gmail.com>
2024-11-07 20:10:11 +0000

    add error handling for deleting comments

--- src/components/CommentItem.jsx
+++ src/components/CommentItem.jsx
@@ -5,11 +5,15 @@ import { deleteCommentById } from "../utils/api";
 const CommentItem = ({ comment }) => {
   const { signedInUser } = useContext(UserContext);
   const [isDeleted, setIsDeleted] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
 
   const handleDelete = () => {
     deleteCommentById(comment.comment_id)
       .then(() => {
         setIsDeleted(true);
+      })
+      .catch(() => {
+        setErrMsg("Error - please try again later");
       })
   }
 
@@ -19,6 +23,7 @@ const CommentItem = ({ comment }) => {
     <li>
       <p><strong>{comment.author}</strong> at {comment.created_at}:</p>
       {signedInUser === comment.author ? <button onClick={handleDelete}>Delete comment</button> : null}
+      <p id="delete-comment-err-msg" className="err-msg">{errMsg}</p>
       <p className="comment">"{comment.body}"</p>
     </li>
   )
